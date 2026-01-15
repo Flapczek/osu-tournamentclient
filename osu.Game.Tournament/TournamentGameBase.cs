84495c05da4aa9840c7c4912eadf3acf97b3064c
Flapczek <kamilotta887@gmail.com>
2026-01-15 17:46:20 +0100

    Update TournamentGameBase.cs

--- osu.Game.Tournament/TournamentGameBase.cs
+++ osu.Game.Tournament/TournamentGameBase.cs
@@ -58,7 +58,7 @@ namespace osu.Game.Tournament
             base.SetHost(host);
 
             if (host.Window != null)
-                host.Window.Title = $"{Name} [tournament client]";
+                host.Window.Title = $"{Name} [conyoh cup 6]";
         }
 
         private TournamentSpriteText initialisationText = null!;
